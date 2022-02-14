eeee97e15fb973b712a85fd06f595acfedb5d4af
Hammad Ul Islam <hammadulislam99@gmail.com>
2022-02-14 11:13:34 +0500

    First Commit

---
+++ api.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const app = express();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post('/',function(req,res){
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+  mailchimp.setConfig({
+    apiKey: "28e17e819341065c8b765959900238d5-us14",
+    server: "us14",
+  });
+  async function run() {
+    try{
+    const res1 = await mailchimp.lists.getAllLists();
+    const id = res1.lists[0].id;
+
+    const res2 = await mailchimp.lists.addListMember(id, {
+    email_address: email,
+    status: "subscribed",
+    merge_fields:{
+      FNAME: fname,
+      LNAME: lname
+    }
+  });
+  res.sendFile(__dirname + "/success.html");
+}
+catch(err) {
+    res.sendFile(__dirname + "/failure.html");
+}
+}
+  run();
+});
+
+
+app.post('/failure',function(req,res){
+  res.redirect('/');
+});
+
+app.listen(process.env.PORT||3000,function(){
+  console.log("Server started!!!");
+});
+
+// e0efcfd4b8
+// 28e17e819341065c8b765959900238d5-us14
+
+
+
+
+
+// app.post('/audience/add/member', async (req, res) => {
+//     const { listId, firstname, lastname, email, tag } = req.body
+// const addListMember = async () => {
+//         try {
+//             const response = await  mailchimp.lists.addListMember(listId, {
+//                 email_address: email,
+//                 status: 'subscribed',
+//                 email_type: 'html',
+//                 merge_fields: {
+//                     FNAME: firstname,
+//                     LNAME: lastname
+//                 },
+//                 tags: [tag]
+//             })
+//             res.send(response)
+//         }
+//         catch (err) {
+//             res.status(400).send(err)
+//         }
+//     }
+//   addListMember()
+// })
